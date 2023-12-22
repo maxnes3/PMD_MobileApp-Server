cae887a63994662f5bcb5a042d2ad199ed5fab73
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2023-12-22 19:16:16 +0400

    Initial commit

---
+++ src/main/java/com/android/mobileappserver/MobileappServerApplication.java
@@ -0,0 +1,13 @@
+package com.android.mobileappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileappServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/android/mobileappserver/MobileappServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.android.mobileappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileappServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
